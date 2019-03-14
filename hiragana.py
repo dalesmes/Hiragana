6c36aeb967c7a9e4d772165cefcf00bd68dd9b47
Duvan Lesmes <dalexislesmes@gmail.com>
2019-03-13 22:14:42 -0500

    Create hiragana.py
    se añadio lista de hirganas

---
+++ hiragana.py
@@ -0,0 +1,4 @@
+import random
+hiraganas = ['a', 'i', 'u', 'e', 'o', 'ka', 'ki', 'ku', 'ke', 'ko', 'sa', 'shi', 'su', 'se', 
+'so', 'ta', 'chi', 'tsu', 'te', 'to', 'na', 'ni', 'nu', 'ne', 'no', 'ha', 'hi', 'fu', 'he', 'ho']
+print("escriba el hiragana", hiraganas[random.randint(0, len(hiraganas)-1)])
