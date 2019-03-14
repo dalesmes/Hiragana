17550c3e999c34432cd07eb83e0f8782a680ca71
Duvan Lesmes <dalexislesmes@gmail.com>
2019-03-13 22:17:18 -0500

    Update hiragana.py
    Se puso la lista en una funcion

--- hiragana.py
+++ hiragana.py
@@ -1,4 +1,7 @@
 import random
-hiraganas = ['a', 'i', 'u', 'e', 'o', 'ka', 'ki', 'ku', 'ke', 'ko', 'sa', 'shi', 'su', 'se', 
-'so', 'ta', 'chi', 'tsu', 'te', 'to', 'na', 'ni', 'nu', 'ne', 'no', 'ha', 'hi', 'fu', 'he', 'ho']
-print("escriba el hiragana", hiraganas[random.randint(0, len(hiraganas)-1)])
+
+def Fun_hiraganas():
+	hiraganas = ['a', 'i', 'u', 'e', 'o', 'ka', 'ki', 'ku', 'ke', 'ko', 'sa', 'shi', 'su', 'se', 
+	'so', 'ta', 'chi', 'tsu', 'te', 'to', 'na', 'ni', 'nu', 'ne', 'no', 'ha', 'hi', 'fu', 'he', 'ho']
+	print("escriba el hiragana", hiraganas[random.randint(0, len(hiraganas)-1)])
+
